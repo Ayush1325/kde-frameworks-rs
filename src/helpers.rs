6d98f51d2b72bc6dcb174c11922da3c4987e4850
Ayush Singh <ayushdevel1325@gmail.com>
2022-01-03 22:07:57 +0530

    Added Comments.

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,18 +1,25 @@
+//! Helper Functions.
+
 use semver::Version;
 use std::path::PathBuf;
 
+/// Helper function. Prefixes `KF{version}` to Library Name.
+/// Eg: `I18n` -> `KF5I18n`
 pub fn get_lib_name(lib: &str) -> Result<String, semver::Error> {
     Ok(format!("KF{}{}", get_kf_version()?.major, lib))
 }
 
+/// Returns KDE Frameworks Root Library Path.
 pub fn get_kf_library_path() -> PathBuf {
     env!("KF_LIBRARY_PATH").into()
 }
 
+/// Returns KDE Frameworks Root Include Path.
 pub fn get_kf_include_path() -> PathBuf {
     env!("KF_INCLUDE_PATH").into()
 }
 
+/// Returns KDE Frameworks Version.
 pub fn get_kf_version() -> Result<Version, semver::Error> {
     env!("KF_VERSION").parse()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,26 @@ pub mod helpers;
 
 use std::path::PathBuf;
 
+/// Function to link a KDE Library.
+/// Eg: For linking `KI18n`:
+/// ```no_run
+/// use kde_frameworks::link_lib;
+///
+/// link_lib("I18n");
+/// ```
 pub fn link_lib(lib: &str) -> Result<(), semver::Error> {
     let lib_name = helpers::get_lib_name(lib);
     println!("cargo:rustc-link-lib={}", lib_name?);
     Ok(())
 }
 
+/// Function to Return the Include Path for the KDE Library.
+/// This is the path where the header files for the Library are located.
+/// ```no_run
+/// use kde_frameworks::get_lib_include_path;
+///
+/// get_lib_include_path("I18n");
+/// ```
 pub fn get_lib_include_path(lib: &str) -> PathBuf {
     helpers::get_kf_include_path().join(format!("K{}", lib))
 }
